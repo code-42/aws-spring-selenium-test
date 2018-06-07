440e8c65191c86dbd76b2e4ec1700743b90b5d18
Edward Dupre <edwd42@gmail.com>
2018-06-07 16:41:56 -0400

    initial commit

---
+++ src/main/java/net/eed/AwsSpringSeleniumTestApplication.java
@@ -0,0 +1,12 @@
+package net.eed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringSeleniumTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpringSeleniumTestApplication.class, args);
+	}
+}
---
+++ src/main/java/net/eed/ServletInitializer.java
@@ -0,0 +1,13 @@
+package net.eed;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AwsSpringSeleniumTestApplication.class);
+	}
+
+}
